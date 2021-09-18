52a23eab09852ad175b2adf2c978683b83ac5cc9
Arghya Sarkar (ASRA) <67339217+sarkarghya@users.noreply.github.com>
2021-09-19 02:15:31 +0530

    Add files via upload

--- GSOC-good-orgs/gsoc_scraped.py
+++ GSOC-good-orgs/gsoc_scraped.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install()) #to just make it work anywhere
 
 #chromedriver_path= r"C:\Users\Admin\chrome_driver\chromedriver.exe"
 #driver = webdriver.Chrome(executable_path=chromedriver_path) #webdriver.Chrome(executable_path=r'C:/path/to/chromedriver.exe')
@@ -12,5 +12,5 @@ driver.get(url)
 time.sleep(15) #if you want to wait 15 seconds for the page to load
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
-orgs = soup.find_all('div', class_ = 'organization-card__name font-black-54')
-print(orgs)
+orgs = soup.find_all('md-card', class_ = 'organization-card _md md-soc-theme flex') #find returns list, attrs returns dict
+url_ids = [data.attrs['data-id'] for data in orgs]
